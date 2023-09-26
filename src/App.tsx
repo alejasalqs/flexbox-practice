e139a69d0979bffa8f49737dd067c3de15892b03
Alejandro Salguero <a.salguero.quiros@accenture.com>
2023-09-26 08:38:15 -0600

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
 import { useState } from 'react';
 import './App.css'
 import Switch from './components/Switch';
+import Navbar from './components/Navbar';
 
 function App() {
   const [componentName, setComponentName] = useState('FlexDirection')
 
   return (
     <>
-      <section>
-        <h1>Flexbox Practice!</h1>
-        <ul>
-          <li>
-            <button className="btn" onClick={() => setComponentName('FlexDirection')}>Flex Direction</button>
-          </li>
-          <li>
-            <button className='btn' onClick={() => setComponentName('JustifyContent')}>Justify Content</button>
-          </li>
-          <li>
-            <button className='btn' onClick={() => setComponentName('AlingContent')}>Aling Items</button>
-          </li>
-        </ul>
-      </section>
-      <section>
-        <Switch componentName={componentName} />
-      </section>
+      <Navbar />
+      <main>
+        <section>
+          <h1>Flexbox Practice!</h1>
+          <ul>
+            <li>
+              <button className="btn" onClick={() => setComponentName('FlexDirection')}>Flex Direction</button>
+            </li>
+            <li>
+              <button className='btn' onClick={() => setComponentName('JustifyContent')}>Justify Content</button>
+            </li>
+            <li>
+              <button className='btn' onClick={() => setComponentName('AlingContent')}>Aling Items</button>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <Switch componentName={componentName} />
+        </section>
+      </main>
     </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <header>
+        <button>Mode</button>
+    </header>
+  )
+}
